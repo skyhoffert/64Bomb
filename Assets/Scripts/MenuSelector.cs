c1f19bc5425fcef735a79b6681ea3a744d132a67
skyhoffert <skwylarh@yahoo.com>
2020-03-13 23:28:17 -0400

    Adding initial files.

---
+++ Assets/Scripts/MenuSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelector : MonoBehaviour
+{
+    public GameObject[] menuItems;
+
+    private int idxSelected;
+    private bool justMoved;
+    private float deadZone;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.idxSelected = 0;
+        this.deadZone = 0.1f;
+        this.justMoved = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.justMoved) {
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) < this.deadZone) {
+                this.justMoved = false;
+            }
+        } else {
+            if (Input.GetAxis("Horizontal") > this.deadZone) {
+                this.justMoved = true;
+                this.idxSelected = (this.idxSelected + 1) % this.menuItems.Length;
+            } else if (Input.GetAxis("Horizontal") < -this.deadZone) {
+                this.justMoved = true;
+                this.idxSelected--;
+                if (this.idxSelected < 0) {
+                    this.idxSelected = this.menuItems.Length - 1;
+                }
+            }
+        }
+
+        if (this.menuItems[this.idxSelected]) {
+            this.menuItems[this.idxSelected].transform.Rotate(0f,100f*Time.deltaTime,0f, Space.Self);
+        }
+    }
+}
